2df64c66103b560721cdf3effd25bb9bc5dd7560
ihym <b.diakomanolis@gmail.com>
2023-04-17 00:53:33 +0300

    feat: add tw + daisy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-base-300">
+	<h1 class="text-base-content">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
